e257e67ea70bf7a18dbf9b7afce69b312120af8a
Greenfin17 <jmaple@yahoo.com>
2021-02-07 22:05:27 -0600

    display pinned repos only if any are pinned

--- main.js
+++ main.js
@@ -14,27 +14,106 @@ const badges = [
       }
  ];
 
+//Array of common technologies
 const technologies = [
 
     { 
       techID: 0,
-      name: "CSS",
-      color: "#563d7c",
-      url: "https://github.com/trending?l=CSS"
+      name: "HTML",
+      color: "#e34c26",
+      url: "https://github.com/trending?l=HTML",
     },
     { 
       techID: 1,
-      name: "HTML",
-      color: "#e34c26",
-      url: "https://github.com/trending?l=HTML"
+      name: "CSS",
+      color: "#563d7c",
+      url: "https://github.com/trending?l=CSS",
     },
     { 
       techID: 2,
       name: "JavaScript",
       color: "#f1e05a",
-      url: "https://github.com/trending?l=JavaScript"
+      url: "https://github.com/trending?l=JavaScript",
+    },
+    { 
+      techID: 3,
+      name: "Apollo Guidance Computer",
+      color: "#0B3D91",
+      url: "https://github.com/trending?l=Apollo-Guidance-Computer",
+    },
+    {
+      techID: 4,
+      name: "AppleScript",
+      color: "#101F1F",
+      url: "https://github.com/trending?l=AppleScript",
+    },
+    {  
+      techID: 5,
+      name: "ASP.NET",
+      color: "#9400ff",
+      url: "https://github.com/trending?l=ASP.NET",
+    },
+    {  
+      techID: 6,
+      name: "C",
+      color: "#555555",
+      url: "https://github.com/trending?l=C",
+    },
+    {   
+      techID: 7,
+      name: "C++",
+      color: "#f34b7d",
+      url: "https://github.com/trending?l=C++",
+    },
+    {  
+      techID: 8,
+      name: "C#",
+      color: "#178600",
+      url: "https://github.com/trending?l=Csharp",
+    },
+    { 
+      techID: 9,
+      name: "Ruby",
+      color: "#701516",
+      url: "https://github.com/trending?l=Ruby",
+    },
+    {
+      techID: 10, 
+      name: "Twig",
+      color: "#c1d026",
+      url: "https://github.com/trending?l=Twig",
     },
 ];
+
+//Array of organizations:
+const organizations_arr = [
+  {
+    orgKey: 0,
+    orgName: "NSS Evening Cohort 14",
+    orgUrl: "https://github.com/nss-evening-cohort-14",
+    orgLogo: "./graphics/Cohort14.jpg",
+  },
+  {
+    orgKey: 1,
+    orgName: "Autism Possible",
+    orgUrl: "https://github.com/Autism-Possisble",
+    orgLogo: "./graphics/AutismPossible.png",
+  },
+  {
+    orgKey: 2,
+    orgName: "Ubran Green Lab",
+    orgUrl: "https://github.com/Urban-Green-Lab",
+    orgLogo: "./graphics/UrbanGreenLab.png",
+  },
+  {
+    orgKey: 3,
+    orgName: "Nashville Software School",
+    orgUrl: "https://github.com/nashville-software-school",
+    orgLogo: "./graphics/NashvilleSoftwareSchool.png",
+  }
+];
+
+
 
 //Array of developers
 const developers_arr = [
@@ -49,9 +128,11 @@ const developers_arr = [
     website: "harrypottershop.com",
     twitterName: "@harryPotterFilm",
     badges: [ 1, 0 ],
+    organizations: [0, 1, 2, 3],
     repositories: [
       {
         repoID: 0,
+        pinned: true,
         repoName: "sorting-hat",
         repoDescription: "Mimics the Sorting Hat of J.K. Rowlings Harry Potter Series." +
                          " Sorts names into the four houses of Hogwarts",
@@ -59,11 +140,48 @@ const developers_arr = [
       },
       {
         repoID: 1,
+        pinned: true,
         repoName: "magic-wand",
         repoDescription: "Challenges the user to a series of questions to see if they are ready to handle a magic wand.",
         technologies: [0, 1, 2],
-      } 
-    ]  //end of Harry Potter's repositories
+      },
+      {
+        repoID: 2,
+        pinned: true,
+        repoName: "broomstick",
+        repoDescription: "Aids user in finding the ideal broomstick, with a list of best candidate sticks and their sellers",
+        technologies: [3, 1, 5],
+      },
+      {
+        repoID: 3,
+        pinned: true,
+        repoName: "spellbound",
+        repoDescription: "Electronic spellbook. Helps user find the right spell in a jiffy. To be used with caution.",
+        technologies: [5, 2, 8],
+      },
+      {
+        repoID: 4,
+        pinned: true,
+        repoName: "tealeaves",
+        repoDescription: "Online ordering of tea, with maps and hours of your favorite tea rooms.",
+        technologies: [0, 1, 2],
+      },
+      { 
+        repoID: 5,
+        pinned: true,
+        repoName: "mentormatic",
+        repoDescription: "App to help upcoming wizards find their best match for a mentor",
+        technologies: [7, 2, 3],
+      },
+      {
+        repoID: 6,
+        pinned: true,
+        repoName: "c yourself",
+        repoDescription: "Tutorial app to aid in learning c.",
+        technologies: [6, 0, 1],
+      }
+    ],  //end of Harry Potter's repositories
+    projects: { }
    }
 ];
 
@@ -96,11 +214,23 @@ const bioHeading = (userBio) => {
 
 const bioBadges = (userBio) => {
   let bioString = 
-    `<h3>Highlights</h3>
+    `<h4>Highlights</h4>
      <ul class="highlights">`;
   for(let i = 0; i< userBio.badges.length; i++) {
     console.log(badges[userBio.badges[i]].badgeName);
     bioString += `<li>${badges[userBio.badges[i]].badgeName}</li>`;
+  }
+  return bioString;
+}
+
+const bioOrganizations = (userBio) => {
+  let bioString =
+    `<h4>Organizations</h4>
+     <ul class="organizations">`;
+  for(let i = 0; i < userBio.organizations.length; i++) {
+    console.log(organizations_arr[userBio.organizations[i]].orgName);
+  //  bioString += `<li>${organizations_arr[userBio.organizations[i]].orgName}</li>`;
+    bioString += `<img src="${organizations_arr[userBio.organizations[i]].orgLogo}" class="orgLogo">`;
   }
   return bioString;
 }
@@ -112,25 +242,60 @@ const paintBio = (userBio) => {
   if(userBio.badges.length) {
     bioString += bioBadges(userBio);
   }
+  if(userBio.organizations.length) {
+    bioString += bioOrganizations(userBio);
+  }
   printToDom('#gitHubProfile', bioString);
 }
 
-const paintPinned = (developer) => {
-  let repoString =  `<h3>Pinned</h3>
-                     <div class="pinned-repos">`;
-  let techIndex = 0;
+const customizePinsButton = () => {
+  let pinString = 
+    `<button type="button" id="customize-pins" class="btn btn-success">Customize your pins</button>`;
+  return pinString;
+}
+
+//See is user has pinned any repositories
+const hasPinned = (developer) => {
+  let pinned = false;
   for(let item of developer.repositories) {
-    techIndex = item.technologies[0];
-    console.log(technologies[techIndex].name);
-    console.log(technologies[techIndex].color);
-      repoString += `<div class="repo-card">
-                        <div class="card-body">
-                          <h3>${item.repoName}</h3>
-                          <p>${item.repoDescription}</p>
-                          <div class="tech-type" style="background-color: ${technologies[techIndex].color}"></div>
-                          <p class="tech-name">${technologies[techIndex].name}</p>
-                        </div>
-                    </div>`;
+    if(item.pinned){
+      pinned = true;
+      break;
+    }
+  }
+  return pinned;
+}
+
+
+//Create respository cards
+const paintPinned = (developer) => {
+  let pinned = "pinned-repose";
+  let  pinButton = customizePinsButton();
+  let repoString = '';
+  if (hasPinned(developer)){
+    repoString =  `<h3>Pinned</h3>${pinButton}
+                       <div class="pinned-repos">`;
+  } else {
+    repoString = `<h3>Repositories</h3>${pinButton}
+                    <div class="unpinned-repos">`;
+  }
+  let techIndex = 0; //tells us the most prominent tech used in the repository
+  let i = 0; //limit to six repositories
+  for(let item of developer.repositories) {
+    if(i < 6) {
+      techIndex = item.technologies[0];
+      //console.log(technologies[techIndex].name);
+      //console.log(technologies[techIndex].color);
+        repoString += `<div class="repo-card">
+                          <div class="card-body">
+                            <h3>${item.repoName}</h3>
+                            <p>${item.repoDescription}</p>
+                            <div class="tech-type" style="background-color: ${technologies[techIndex].color}"></div>
+                            <p class="tech-name">${technologies[techIndex].name}</p>
+                          </div>
+                      </div>`;
+      i++; //limit to six repositories
+    } 
   }
   repoString += `</div>`;
   printToDom('#gitHubRepos', repoString);
