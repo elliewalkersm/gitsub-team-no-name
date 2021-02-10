5f286172f0d69d25a5426e60a65a72e6599389c2
Ben Jarrett <b.cl.jarrett@gmail.com>
2021-02-09 19:53:02 -0600

    created form that adds repo

--- main.js
+++ main.js
@@ -1,88 +1,87 @@
-console.log("Team No-Name");
+// console.log("Team No-Name");
 
 //Array of common Badges / Highlights
 const badges = [
-      {
-        badgeID: 0,
-        badgeName: "Arctic Code Vault Contributor",
-        badgeLogo: "./graphics/Arctic.svg"
-      },
-      {
-        badgeID: 1,
-        badgeName: "Developer Program Member",
-        badgeLogo: "./graphics/Developer.svg"
-      }
- ];
+  {
+    badgeID: 0,
+    badgeName: "Arctic Code Vault Contributor",
+    badgeLogo: "./graphics/Arctic.svg",
+  },
+  {
+    badgeID: 1,
+    badgeName: "Developer Program Member",
+    badgeLogo: "./graphics/Developer.svg",
+  },
+];
 
 //Array of common technologies
 const technologies = [
-
-    { 
-      techID: 0,
-      name: "HTML",
-      color: "#e34c26",
-      url: "https://github.com/trending?l=HTML",
-    },
-    { 
-      techID: 1,
-      name: "CSS",
-      color: "#563d7c",
-      url: "https://github.com/trending?l=CSS",
-    },
-    { 
-      techID: 2,
-      name: "JavaScript",
-      color: "#f1e05a",
-      url: "https://github.com/trending?l=JavaScript",
-    },
-    { 
-      techID: 3,
-      name: "Apollo Guidance Computer",
-      color: "#0B3D91",
-      url: "https://github.com/trending?l=Apollo-Guidance-Computer",
-    },
-    {
-      techID: 4,
-      name: "AppleScript",
-      color: "#101F1F",
-      url: "https://github.com/trending?l=AppleScript",
-    },
-    {  
-      techID: 5,
-      name: "ASP.NET",
-      color: "#9400ff",
-      url: "https://github.com/trending?l=ASP.NET",
-    },
-    {  
-      techID: 6,
-      name: "C",
-      color: "#555555",
-      url: "https://github.com/trending?l=C",
-    },
-    {   
-      techID: 7,
-      name: "C++",
-      color: "#f34b7d",
-      url: "https://github.com/trending?l=C++",
-    },
-    {  
-      techID: 8,
-      name: "C#",
-      color: "#178600",
-      url: "https://github.com/trending?l=Csharp",
-    },
-    { 
-      techID: 9,
-      name: "Ruby",
-      color: "#701516",
-      url: "https://github.com/trending?l=Ruby",
-    },
-    {
-      techID: 10, 
-      name: "Twig",
-      color: "#c1d026",
-      url: "https://github.com/trending?l=Twig",
-    },
+  {
+    techID: 0,
+    name: "HTML",
+    color: "#e34c26",
+    url: "https://github.com/trending?l=HTML",
+  },
+  {
+    techID: 1,
+    name: "CSS",
+    color: "#563d7c",
+    url: "https://github.com/trending?l=CSS",
+  },
+  {
+    techID: 2,
+    name: "JavaScript",
+    color: "#f1e05a",
+    url: "https://github.com/trending?l=JavaScript",
+  },
+  {
+    techID: 3,
+    name: "Apollo Guidance Computer",
+    color: "#0B3D91",
+    url: "https://github.com/trending?l=Apollo-Guidance-Computer",
+  },
+  {
+    techID: 4,
+    name: "AppleScript",
+    color: "#101F1F",
+    url: "https://github.com/trending?l=AppleScript",
+  },
+  {
+    techID: 5,
+    name: "ASP.NET",
+    color: "#9400ff",
+    url: "https://github.com/trending?l=ASP.NET",
+  },
+  {
+    techID: 6,
+    name: "C",
+    color: "#555555",
+    url: "https://github.com/trending?l=C",
+  },
+  {
+    techID: 7,
+    name: "C++",
+    color: "#f34b7d",
+    url: "https://github.com/trending?l=C++",
+  },
+  {
+    techID: 8,
+    name: "C#",
+    color: "#178600",
+    url: "https://github.com/trending?l=Csharp",
+  },
+  {
+    techID: 9,
+    name: "Ruby",
+    color: "#701516",
+    url: "https://github.com/trending?l=Ruby",
+  },
+  {
+    techID: 10,
+    name: "Twig",
+    color: "#c1d026",
+    url: "https://github.com/trending?l=Twig",
+  },
 ];
 
 //Array of organizations:
@@ -110,15 +109,13 @@ const organizations_arr = [
     orgName: "Nashville Software School",
     orgUrl: "https://github.com/nashville-software-school",
     orgLogo: "./graphics/NashvilleSoftwareSchool.png",
-  }
+  },
 ];
-
-
 
 //Array of developers
 const developers_arr = [
   {
-    keyId : 0, 
+    keyId: 0,
     name: "Harry Potter",
     photo: "./images/HarryPotter.jpg",
     gitHubName: "MagicHarry",
@@ -127,50 +124,56 @@ const developers_arr = [
     geoLocation: "Gryffindor at Hogwarts",
     website: "harrypottershop.com",
     twitterName: "@harryPotterFilm",
-    badges: [ 1, 0 ],
+    badges: [1, 0],
     organizations: [0, 1, 2, 3],
     repositories: [
       {
         repoID: 0,
         pinned: true,
         repoName: "sorting-hat",
-        repoDescription: "Mimics the Sorting Hat of J.K. Rowlings Harry Potter Series." +
-                         " Sorts names into the four houses of Hogwarts",
-        technologies: [1, 0, 2], 
+        repoDescription:
+          "Mimics the Sorting Hat of J.K. Rowlings Harry Potter Series." +
+          " Sorts names into the four houses of Hogwarts",
+        technologies: [1, 0, 2],
       },
       {
         repoID: 1,
         pinned: true,
         repoName: "magic-wand",
-        repoDescription: "Challenges the user to a series of questions to see if they are ready to handle a magic wand.",
+        repoDescription:
+          "Challenges the user to a series of questions to see if they are ready to handle a magic wand.",
         technologies: [0, 1, 2],
       },
       {
         repoID: 2,
         pinned: true,
         repoName: "broomstick",
-        repoDescription: "Aids user in finding the ideal broomstick, with a list of best candidate sticks and their sellers",
+        repoDescription:
+          "Aids user in finding the ideal broomstick, with a list of best candidate sticks and their sellers",
         technologies: [3, 1, 5],
       },
       {
         repoID: 3,
         pinned: true,
         repoName: "spellbound",
-        repoDescription: "Electronic spellbook. Helps user find the right spell in a jiffy. To be used with caution.",
+        repoDescription:
+          "Electronic spellbook. Helps user find the right spell in a jiffy. To be used with caution.",
         technologies: [5, 2, 8],
       },
       {
         repoID: 4,
         pinned: true,
         repoName: "tealeaves",
-        repoDescription: "Online ordering of tea, with maps and hours of your favorite tea rooms.",
+        repoDescription:
+          "Online ordering of tea, with maps and hours of your favorite tea rooms.",
         technologies: [0, 1, 2],
       },
-      { 
+      {
         repoID: 5,
         pinned: true,
         repoName: "mentormatic",
-        repoDescription: "App to help upcoming wizards find their best match for a mentor",
+        repoDescription:
+          "App to help upcoming wizards find their best match for a mentor",
         technologies: [7, 2, 3],
       },
       {
@@ -179,101 +182,103 @@ const developers_arr = [
         repoName: "c yourself",
         repoDescription: "Tutorial app to aid in learning c.",
         technologies: [6, 0, 1],
-      }
-    ],  //end of Harry Potter's repositories
-    projects: { }
-   }
+      },
+    ], //end of Harry Potter's repositories
+    projects: {},
+  },
 ];
 
+const repos_Arr = [
+{
+
+},
+];
 
 const printToDom = (ID, string) => {
   document.querySelector(ID).innerHTML = string;
-}
+};
 
 const bioHeading = (userBio) => {
-  let bioString = 
-   `  <img src="${userBio.photo}" alt="photo of ${userBio.name}">
+  let bioString = `  <img src="${userBio.photo}" alt="photo of ${userBio.name}">
       <h2>${userBio.name}</h2>
       <h3>${userBio.gitHubName}</h3>
       </p>${userBio.bio}</p>
       <ul class="bio-list">`;
-  if(userBio.company != '') {
+  if (userBio.company != "") {
     bioString += `<li>${userBio.company}</li>`;
   }
-  if(userBio.egoLocation != '') {
+  if (userBio.egoLocation != "") {
     bioString += `<li>${userBio.geoLocation}</li>`;
   }
-  if(userBio.website != '') {
+  if (userBio.website != "") {
     bioString += `<li>${userBio.website}</li>`;
   }
-  if(userBio.twitterName!= '') {
+  if (userBio.twitterName != "") {
     bioString += `<li>${userBio.twitterName}</li>`;
   }
   return bioString;
-}
+};
 
 const bioBadges = (userBio) => {
-  let bioString = 
-    `<h4>Highlights</h4>
+  let bioString = `<h4>Highlights</h4>
      <ul class="highlights">`;
-  for(let i = 0; i< userBio.badges.length; i++) {
-    console.log(badges[userBio.badges[i]].badgeName);
+  for (let i = 0; i < userBio.badges.length; i++) {
+    badges[userBio.badges[i]].badgeName;
     bioString += `<li>${badges[userBio.badges[i]].badgeName}</li>`;
   }
   return bioString;
-}
+};
 
 const bioOrganizations = (userBio) => {
-  let bioString =
-    `<h4>Organizations</h4>
+  let bioString = `<h4>Organizations</h4>
      <ul class="organizations">`;
-  for(let i = 0; i < userBio.organizations.length; i++) {
-    console.log(organizations_arr[userBio.organizations[i]].orgName);
-  //  bioString += `<li>${organizations_arr[userBio.organizations[i]].orgName}</li>`;
-    bioString += `<img src="${organizations_arr[userBio.organizations[i]].orgLogo}" class="orgLogo">`;
+  for (let i = 0; i < userBio.organizations.length; i++) {
+    organizations_arr[userBio.organizations[i]].orgName;
+    //  bioString += `<li>${organizations_arr[userBio.organizations[i]].orgName}</li>`;
+    bioString += `<img src="${
+      organizations_arr[userBio.organizations[i]].orgLogo
+    }" class="orgLogo">`;
   }
   return bioString;
-}
+};
 
 //Create side-bar bio section
 const paintBio = (userBio) => {
-  let bioString = bioHeading(userBio); 
+  let bioString = bioHeading(userBio);
   //Display Highlights / Badges
-  if(userBio.badges.length) {
+  if (userBio.badges.length) {
     bioString += bioBadges(userBio);
   }
-  if(userBio.organizations.length) {
+  if (userBio.organizations.length) {
     bioString += bioOrganizations(userBio);
   }
-  printToDom('#gitHubProfile', bioString);
-}
+  printToDom("#gitHubProfile", bioString);
+};
 
 const customizePinsButton = () => {
-  let pinString = 
-    `<button type="button" id="customize-pins" class="btn btn-success">Customize your pins</button>`;
+  let pinString = `<button type="button" id="customize-pins" class="btn btn-success">Customize your pins</button>`;
   return pinString;
-}
+};
 
 //See is user has pinned any repositories
 const hasPinned = (developer) => {
   let pinned = false;
-  for(let item of developer.repositories) {
-    if(item.pinned){
+  for (let item of developer.repositories) {
+    if (item.pinned) {
       pinned = true;
       break;
     }
   }
   return pinned;
-}
-
+};
 
 //Create respository cards
 const paintPinned = (developer) => {
   let pinned = "pinned-repose";
-  let  pinButton = customizePinsButton();
-  let repoString = '';
-  if (hasPinned(developer)){
-    repoString =  `<h3>Pinned</h3>${pinButton}
+  let pinButton = customizePinsButton();
+  let repoString = "";
+  if (hasPinned(developer)) {
+    repoString = `<h3>Pinned</h3>${pinButton}
                        <div class="pinned-repos">`;
   } else {
     repoString = `<h3>Repositories</h3>${pinButton}
@@ -281,12 +286,12 @@ const paintPinned = (developer) => {
   }
   let techIndex = 0; //tells us the most prominent tech used in the repository
   let i = 0; //limit to six repositories
-  for(let item of developer.repositories) {
-    if(i < 6) {
+  for (let item of developer.repositories) {
+    if (i < 6) {
       techIndex = item.technologies[0];
       //console.log(technologies[techIndex].name);
       //console.log(technologies[techIndex].color);
-        repoString += `<div class="repo-card">
+      repoString += `<div class="repo-card">
                           <div class="card-body">
                             <h3>${item.repoName}</h3>
                             <p>${item.repoDescription}</p>
@@ -295,16 +300,39 @@ const paintPinned = (developer) => {
                           </div>
                       </div>`;
       i++; //limit to six repositories
-    } 
+    }
   }
   repoString += `</div>`;
-  printToDom('#gitHubRepos', repoString);
-}
+  printToDom("#gitHubRepos", repoString);
+};
+
+
+const newRepo = (repoID) => {
+  let createRepoString = "";
+  createRepoString = `<form>
+  <div class="form-group">
+    <label for="nameFormControlInput1">Repository name</label>
+    <input type="name" class="form-control" id="createRepoName" value="" required></div>
+  <div class="form-group">
+    <label for="descriptionText">Description (optional)</label>
+    <textarea class="form-control" id="descriptionBox" rows="2"></textarea>
+  </div>
+  <hr></hr>
+  <button type="submit" id="repoSubmit" class="btn btn-success">Create repository</button>
+</form>`;
+
+  printToDom("#createNewRepo", createRepoString);
+};
+
+
+const createdRepos = (repoID) => {
+  let repoContainer = "";
+  repoContainer = ``
+} 
 
 const newProject = (developerId) => {
-  let projStr = '';
-  projStr = 
-    `<div class="container">
+  let projStr = "";
+  projStr = `<div class="container">
      <p>Coordinate, track, and update your work in one place, so projects stay transparent and on schedule</p>
       <h2>Create a new project</h2>
       <label for="project-name">Project board name</label>
@@ -315,25 +343,46 @@ const newProject = (developerId) => {
     </div>`;
 
   printToDom("#new-project", projStr);
-}
+};
 
 const projectSubmit = (e) => {
-  if(e.target.id == 'project-submit') {
-    console.log(document.querySelector('#project-name').value);
-    console.log(document.querySelector('#project-description').value);
+  if (e.target.id == "project-submit") {
+    console.log(document.querySelector("#project-name").value);
+    console.log(document.querySelector("#project-description").value);
   }
-}
+};
 
 const buttonListener = () => {
-  document.getElementById('new-project').addEventListener('click', projectSubmit);
-}
-   
+  document
+    .getElementById("new-project")
+    .addEventListener("click", projectSubmit)
+};
 
 const init = () => {
-  paintBio(developers_arr[0]);
-  paintPinned(developers_arr[0]);
-  newProject(0);
-  buttonListener();
-}
+  const page = (window.location.pathname)
+  switch (page) {
+    case "/index.html":
+    paintPinned(developers_arr[0]);
+    newProject(0);
+    break;
 
-init(); 
+    case "/repositories.html":
+    newRepo(repos_Arr)
+    break;
+
+    case "/projects.html":
+      newProject(0)
+      break;
+
+    case "/packages.html":
+      buildPackages(packages_arr);
+        break; 
+  };
+  
+  paintBio(developers_arr[0]);
+  buttonListener();
+
+
+};
+
+init();
